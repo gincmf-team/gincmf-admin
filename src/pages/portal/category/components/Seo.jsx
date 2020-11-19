92bef116938c94e289dfa0dde5710938fe142cc0
1140444693@qq.com <1140444693@qq.com>
2020-11-19 20:05:44 +0800

    验证eslint

--- src/pages/portal/category/components/Seo.jsx
+++ src/pages/portal/category/components/Seo.jsx
@@ -1,52 +1,47 @@
-import react,{useRef,useEffect} from "React";
+import React, { useEffect } from "react";
 import { Form, Input, Row, Col } from "antd";
 
 const formItemLayout = {
-    labelCol: {
-        xs: { span: 24 },
-        sm: { span: 4 },
-    },
-    wrapperCol: {
-        xs: { span: 24 },
-        sm: { span: 19 },
-    },
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 4 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 19 },
+  },
 };
 
-
 const Seo = (props) => {
+  const { onFormChange, form, formData } = props;
 
-    const { onFormChange, form ,formData} = props
+  useEffect(() => {
+    console.log(formData);
+    // 编辑
+    if (formData) {
+      form.setFieldsValue(formData);
+    }
+  }, [formData]);
 
-    useEffect(() => {
-        console.log(formData)
-        // 编辑
-        if (formData) {
-            form.setFieldsValue(formData);
-        }
+  return (
+    <Row>
+      <Col span={12}>
+        <Form form={form} onValuesChange={onFormChange} {...formItemLayout}>
+          <Form.Item label="SEO标题" name="seo_title">
+            <Input />
+          </Form.Item>
 
-    }, [formData])
+          <Form.Item label="SEO关键字" name="seo_keywords">
+            <Input />
+          </Form.Item>
 
-    return (
-        <Row>
-            <Col span={12}>
-                <Form form={form} onValuesChange={onFormChange} {...formItemLayout}>
-                    
-                    <Form.Item label="SEO标题" name="seo_title">
-                        <Input />
-                    </Form.Item>
+          <Form.Item label="SEO描述" name="seo_description">
+            <Input.TextArea rows={4} />
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
+  );
+};
 
-                    <Form.Item label="SEO关键字" name="seo_keywords">
-                        <Input />
-                    </Form.Item>
-
-                    <Form.Item label="SEO描述" name="seo_description">
-                        <Input.TextArea rows={4}/>
-                    </Form.Item>
-
-                </Form>
-            </Col>
-        </Row>
-    );
-}
-
-export default Seo
+export default Seo;
